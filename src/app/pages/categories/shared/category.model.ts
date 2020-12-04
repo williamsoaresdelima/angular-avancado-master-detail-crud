54a9222cfe24fcc07ca232055bc611f7e1c1ad65
williamsoaresdelima <43095193+williamsoaresdelima@users.noreply.github.com>
2020-12-03 21:10:11 -0300

    improving BaseResourceService && refactoring CategoryService

--- src/app/pages/categories/shared/category.model.ts
+++ src/app/pages/categories/shared/category.model.ts
@@ -1,11 +1,15 @@
-import { BaseResourceModel  } from "../../../shared/models/base-resource.model";
+import { BaseResourceModel } from "../../../shared/models/base-resource.model";
 
-export class Category extends BaseResourceModel{
-    constructor(
-        public id?:number,
-        public name?:string,
-        public description?:string
-    ){
-      super();
-    }
+export class Category extends BaseResourceModel {
+  constructor(
+    public id?:number,
+    public name?: string,
+    public description?: string
+  ){
+    super();
+  }
+  
+  static fromJson(jsonData: any): Category {
+    return Object.assign(new Category(), jsonData);
+  }
 }
