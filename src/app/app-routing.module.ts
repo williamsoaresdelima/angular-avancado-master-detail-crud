122e38021142350734efc605e2ce69e7524309ea
williamsoaresdelima <43095193+williamsoaresdelima@users.noreply.github.com>
2020-12-04 21:24:41 -0300

    add Reports base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
   { path: 'entries', loadChildren:()=>import('./pages/entries/entries.module').then(m => m.EntriesModule)},
-  { path: 'categories', loadChildren:()=>import('./pages/categories/categories.module').then(m => m.CategoriesModule)  }
+  { path: 'categories', loadChildren:()=>import('./pages/categories/categories.module').then(m => m.CategoriesModule)},
+  { path: 'reports', loadChildren:()=>import('./pages/reports/reports.module').then(m => m.ReportsModule)},
+  { path: '', loadChildren:()=>import('./pages/reports/reports.module').then(m => m.ReportsModule)}
 ];
-
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
